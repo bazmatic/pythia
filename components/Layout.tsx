cef5051ff0d42bc07a93fe66c58a1fd02385c427
Barry Earsman <barry@earsman.com>
2024-09-26 21:57:45 +1000

    feat: layout

---
+++ components/Layout.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Link from 'next/link';
+
+interface LayoutProps {
+  children: React.ReactNode;
+}
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  return (
+    <div className="layout">
+      <header>
+        <Link href="/" className="home-link">
+          <span className="home-text">Home</span>
+        </Link>
+        <Link href="/stats" className="home-link">History</Link>
+      </header>
+      <main>
+        {children}
+      </main>
+    </div>
+  );
+};
+
+export default Layout;
